35a867399d6e2d7e486bd37245ad997e7772c258
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2022-10-19 01:02:57 +0100

    Change in HelloConsole script
    Added comment and student number to print HelloConsole.

--- Assets/HelloConsole.cs
+++ Assets/HelloConsole.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class HelloConsole : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //write to console
     void Start()
     {
-        Debug.Log("Hello Console");   
+        Debug.Log("2239356");   
     }
 
     // Update is called once per frame
